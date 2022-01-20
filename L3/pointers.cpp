e71a997fd5be3f64e854280c6a6e444340f29163
RIYA RAI <riya20rai@gmail.com>
2022-01-20 21:03:05 +0530

    Added Lecture 2

---
+++ L3/pointers.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a=10;
+
+    int* ptr;
+    ptr = &a;
+    int** ptrptr = &ptr;
+    int*** ptrptrptr = &ptrptr;
+
+    cout<<"a: "<<a<<endl;
+    cout<<"*ptr: "<<*ptr<<endl;
+    cout<<"**ptrptr: "<<**ptrptr<<endl;
+
+cout<<endl;
+
+    cout<<"&a: "<<&a<<endl;
+    cout<<"ptr: "<<ptr<<endl;
+    cout<<"*ptrptr: "<<*ptrptr<<endl;
+
+cout<<endl;
+
+    cout<<"&ptr: "<<&ptr<<endl;
+    cout<<"ptrptr: "<<ptrptr<<endl;
+
+}
